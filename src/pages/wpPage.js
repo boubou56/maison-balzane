f399ecbbf80c51948e1e03c19dba62b5cb48845a
Boucar <Boucar@BOUCAR.local>
2022-05-20 16:07:39 +0200

    2005

---
+++ src/pages/wpPage.js
@@ -0,0 +1,59 @@
+
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from '../../components/layout';
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import parse from "html-react-parser"
+import { titrepage, 
+        contenupage,
+  } from '../../css/Page.module.css'
+
+export default function Page(props) {
+
+  const {//champs de premier niveau
+    id, title, content
+  } = props?.data?.wpPage;
+
+  const lestitres = Page.nodes.map(function(elementPage){
+    return {
+        title: elementPage.title,
+    }
+  })
+
+  const lescontenus = Page.nodes.map(function(elementPage){
+    return {
+        title: elementPage.title,
+
+    }
+  })
+
+  console.log (props?.data?.wpPage)
+
+  return (
+    <Layout>
+      <h2 className={titrepage}>
+      
+              <PageTitlePage title={lestitres} />
+     
+      </h2>
+
+      <div className={contenupage}>
+              <PageContentPage content={lescontenus}/>
+      </div>
+
+    </Layout>
+    )
+  }
+// This is the page query that connects the data to the actual component. Here you can query for any and all fields
+// you need access to within your code. Again, since Gatsby always queries for `id` in the collection, you can use that
+// to connect to this GraphQL query.
+export const query = graphql`
+  query($id: String) {
+    wpPage(id: { eq: $id }) {
+    nodes {
+      content
+      title
+    }
+  }
+  }
+`
